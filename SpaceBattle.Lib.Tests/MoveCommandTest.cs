36c8d66605cc38bb4b0367728e1e1dfae02dfc7f
Ivan Sharun <ivan@sha.run>
2023-09-15 16:57:51 +0600

    ПЗ №2

---
+++ SpaceBattle.Lib.Tests/MoveCommandTest.cs
@@ -0,0 +1,25 @@
+﻿namespace SpaceBattle.Lib.Tests;
+using Moq;
+
+public class MoveCommandTest
+{
+    [Fact]
+    public void MoveCommandPositive()
+    {
+        //pre
+
+        var movable = new Mock<IMovable>();
+        movable.SetupGet(m => m.Position).Returns(new int[] { 12, 5 }).Verifiable();
+        movable.SetupGet(m => m.Velocity).Returns(new int[] { -5, 3 }).Verifiable();
+        var mc = new MoveCommand(movable.Object);
+
+        //act
+        mc.Execute();
+
+        //post
+        //movable // pos == (7, 8)
+
+        movable.VerifySet(m => m.Position = new int[] { 7, 8 }, Times.Once);
+        movable.VerifyAll();
+    }
+}
---
+++ SpaceBattle.Lib/Move.cs
@@ -0,0 +1,24 @@
+﻿namespace SpaceBattle.Lib;
+
+public interface IMovable
+{
+    public int[] Position { get; set; }
+    public int[] Velocity { get; }
+}
+
+public class MoveCommand : ICommand
+{
+    private readonly IMovable movable;
+    public MoveCommand(IMovable movable)
+    {
+        this.movable = movable;
+    }
+    public void Execute()
+    {
+        movable.Position = new int[] {
+            movable.Position[0] + movable.Velocity[0],
+            movable.Position[1] + movable.Velocity[1],
+        };
+
+    }
+}
