ce8224647fd82f4e24acb26c8f0263313f950b10
zaryanezrya <ivan@sha.run>
2023-10-27 17:38:28 +0600

    Add Move and Rotate BDD

---
+++ SpaceBattle.Lib.Tests/StepDefinitions/Move.cs
@@ -0,0 +1,63 @@
+﻿using Moq;
+using TechTalk.SpecFlow;
+
+namespace SpaceBattle.Lib.Tests;
+
+[Binding]
+public class Move
+{
+    private readonly ScenarioContext _scenarioContext;
+    private readonly Mock<IMovable> _movable;
+
+    private Action commandExecutionLambda;
+
+    public Move(ScenarioContext scenarioContext)
+    {
+        _scenarioContext = scenarioContext;
+
+        _movable = new Mock<IMovable>();
+
+        commandExecutionLambda = () => { };
+
+    }
+
+    [Given(@"космический корабль находится в точке пространства с координатами \((.*), (.*)\)")]
+    public void ДопустимКосмическийКорабльНаходитсяВТочкеПространстваСКоординатами(int p0, int p1)
+    {
+        _movable.SetupGet(m => m.Position).Returns(new int[] { p0, p1 });
+    }
+
+    [Given(@"имеет мгновенную скорость \((.*), (.*)\)")]
+    public void ДопустимИмеетМгновеннуюСкорость(int p0, int p1)
+    {
+        _movable.SetupGet(m => m.Velocity).Returns(new int[] { p0, p1 });
+    }
+
+    [When(@"происходит прямолинейное равномерное движение без деформации")]
+    public void КогдаПроисходитПрямолинейноеРавномерноеДвижениеБезДеформации()
+    {
+        var mc = new MoveCommand(_movable.Object);
+        commandExecutionLambda = () => mc.Execute();
+    }
+
+    [Then(@"космический корабль перемещается в точку пространства с координатами \((.*), (.*)\)")]
+    public void ТоКосмическийКорабльПеремещаетсяВТочкуПространстваСКоординатами(int p0, int p1)
+    {
+        commandExecutionLambda();
+        _movable.VerifySet(m => m.Position = new int[] { p0, p1 }, Times.Once);
+    }
+
+    [Given(@"космический корабль, положение в пространстве которого невозможно определить")]
+    public void ДопустимКосмическийКорабльПоложениеВПространствеКоторогоНевозможноОпределить()
+    {
+        _movable.SetupGet(m => m.Position).Throws<Exception>();
+        // _movable.SetupGet(m => m.Position).Returns(new int[] { 1, 1 });
+    }
+
+    [Then(@"возникает ошибка Exception")]
+    public void ТоВозникаетОшибкаException()
+    {
+        Assert.Throws<Exception>(() => commandExecutionLambda());
+
+    }
+}
